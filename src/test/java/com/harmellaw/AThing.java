93442807d846cb7177429e7b571cb53416ac04e6
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-04 12:48:54 +0100

    Add starter gradle config, starter (empty) JUnit5 test, and ignore IntelliJ IDEA config files.

---
+++ src/test/java/com/harmellaw/AThing.java
@@ -0,0 +1,23 @@
+package com.harmellaw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AThing {
+
+    @BeforeEach
+    public void setup() {
+        // Setup to run before each test
+    }
+
+    @Test
+    public void shouldBeSomething() {
+        // Make assertions
+    }
+
+    @Test
+    public void shouldDoSomethingElse_whenSomethingHappens() {
+        // Make assertions
+    }
+
+}
