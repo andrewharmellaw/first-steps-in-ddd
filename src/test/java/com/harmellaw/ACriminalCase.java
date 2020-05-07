3ea41205a77802908dfe573326c59ebee4b8200a
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-07 14:03:12 +0100

    Add (empty) example ACriminalCase.java

---
+++ src/test/java/com/harmellaw/ACriminalCase.java
@@ -0,0 +1,24 @@
+
+package com.harmellaw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ACriminalCase {
+
+    @BeforeEach
+    public void setup() {
+    }
+
+    @Test
+    public void shouldDoSomething() {
+        fail();
+    }
+    
+    @Test
+    public void shoudDoSomething_whenSomethingElseHappens() {
+        fail();
+    }
+}
