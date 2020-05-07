3e74fe36a81042a870d305be00ee418de8c3c9e2
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-07 13:39:49 +0100

    Add example PoliceInvestigation.java

---
+++ src/main/java/com/harmellaw/PoliceInvestigation.java
@@ -0,0 +1,13 @@
+package com.harmellaw;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PoliceInvestigation {
+    public Set<Suspect> suspects = new HashSet<>();
+
+    public Investigation(Suspect aSuspect) {
+        if (aSuspect == null) throw new IllegalArgumentException("You must provide a suspect");
+        suspects.add(aSuspect);
+    }
+}
