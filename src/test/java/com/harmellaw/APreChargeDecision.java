450b12ca1cf9b3fb2b955ba1014028ea920cb72b
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-07 14:01:04 +0100

    Add example APreChargeDecision.java test

---
+++ src/test/java/com/harmellaw/APreChargeDecision.java
@@ -0,0 +1,23 @@
+package com.harmellaw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class APreChargeDecision {
+
+    @BeforeEach
+    public void setup() {
+    }
+
+    @Test
+    public void shouldRecordAlternativeCharge_AdviceAgainstSuspects() {
+        fail();
+    }
+    
+    @Test
+    public void shoudRecordDisclosureManagementReminders() {
+        fail();
+    }
+}
