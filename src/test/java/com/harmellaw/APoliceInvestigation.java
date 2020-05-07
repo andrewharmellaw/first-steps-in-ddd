98df7ac1c40b83f06cd0e5db72e7eea1b5e2ad54
Andrew Harmel-Law <andrew@harmel-law.com>
2020-05-07 13:40:51 +0100

    Change example test name

--- src/test/java/com/harmellaw/APoliceInvestigation.java
+++ src/test/java/com/harmellaw/APoliceInvestigation.java
@@ -3,7 +3,7 @@ package com.harmellaw;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class AThing {
+public class APoliceInvestigation {
 
     @BeforeEach
     public void setup() {
