d6ad09aa6b87c6d90915b6796663d2f5f3ce50d7
Andrew Harmel-Law <andrew@harmel-law.com>
2020-06-26 12:20:10 +0100

    Remove line which was too long for Checkstyle rule.

--- src/main/java/com/harmellaw/CriminalOffence.java
+++ src/main/java/com/harmellaw/CriminalOffence.java
@@ -15,7 +15,6 @@ public enum CriminalOffence {
     FALSE_EVIDENCE_BEFORE_EUROPEAN_COURT("False evidence before European Court"),
     FIRING_ON_REVENUE_VESSEL("Firing on Revenue vessel"),
     FRAUDULENT_EVASION_OF_AGRICULTURAL_LEVY("Fraudulent evasion of agricultural levy"),
-    MISHANDLING_OR_FALSIFYING_PARKING_DOCUMENTS_ETC("Mishandling or falsifying parking documents etc"),
     OBSTRUCTING_ENGINE_OR_CARRIAGE_ON_RAILWAY("Obstructing engine or carriage on railway");
 
     private final String value;
